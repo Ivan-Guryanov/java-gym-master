0be5999d60d0fad1041a7a9e3b0145397816850b
ivan <i180388@yandex.ru>
2025-11-30 17:04:46 +0300

    rev1.01 Решение на проверку

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,52 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+
+        TreeMap<TimeOfDay, ArrayList<TrainingSession>> sessionsForDay =
+                timetable.computeIfAbsent(day, k -> new TreeMap<>());
+
+        ArrayList<TrainingSession> sessionsAtSpecificTime =
+                sessionsForDay.computeIfAbsent(time, k -> new ArrayList<>());
+
+        sessionsAtSpecificTime.add(trainingSession);
         //сохраняем занятие в расписании
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public TreeMap<TimeOfDay, ArrayList<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.get(dayOfWeek);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    public ArrayList<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+
+        TreeMap<TimeOfDay, ArrayList<TrainingSession>> sessionsForDay = timetable.get(dayOfWeek);
+
+        if (sessionsForDay != null) {
+            return sessionsForDay.get(timeOfDay);
+        }
+
+        return null;
     }
+
+    public HashMap<Coach, Integer> getCountByCoaches() {
+        HashMap<Coach, Integer> coachCounts = new HashMap<>();
+
+        for (TreeMap<TimeOfDay, ArrayList<TrainingSession>> day : timetable.values()) {
+            if (day == null) continue;
+            for (ArrayList<TrainingSession> sessionsAtTime : day.values()) {
+                for (TrainingSession session : sessionsAtTime) {
+                    Coach coach = session.getCoach(); // Получаем тренера текущего занятия
+                    coachCounts.computeIfAbsent(coach, k -> 0);
+                    coachCounts.put(coach, coachCounts.get(coach) + 1);
+
+                }
+            }
+        }
+        return coachCounts;
+    }
+
 }
---
+++ src/ru/yandex/practicum/gym/practicum.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.gym;
+
+public class practicum {
+    public static void main(String[] args) {
+        Timetable timetable = new Timetable();
+
+        Group group = new Group("Акробатика для детей", Age.CHILD, 60);
+        Group groupAdult = new Group("Акробатика для взрослых", Age.ADULT, 90);
+        Group groupChild = new Group("Акробатика для детей", Age.CHILD, 60);
+
+        Coach coach = new Coach("Васильев", "Николай", "Сергеевич");
+
+
+        TrainingSession thursdayAdultTrainingSession = new TrainingSession(groupAdult, coach,
+                DayOfWeek.THURSDAY, new TimeOfDay(20, 0));
+        TrainingSession singleTrainingSession = new TrainingSession(group, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession mondayChildTrainingSession = new TrainingSession(groupChild, coach,
+                DayOfWeek.MONDAY, new TimeOfDay(13, 0));
+        TrainingSession thursdayChildTrainingSession = new TrainingSession(groupChild, coach,
+                DayOfWeek.THURSDAY, new TimeOfDay(13, 0));
+        TrainingSession saturdayChildTrainingSession = new TrainingSession(groupChild, coach,
+                DayOfWeek.SATURDAY, new TimeOfDay(10, 0));
+
+        timetable.addNewTrainingSession(singleTrainingSession);
+        timetable.addNewTrainingSession(thursdayAdultTrainingSession);
+        timetable.addNewTrainingSession(mondayChildTrainingSession);
+        timetable.addNewTrainingSession(thursdayChildTrainingSession);
+        timetable.addNewTrainingSession(saturdayChildTrainingSession);
+
+        System.out.println(timetable);
+
+    }
+
+
+
+}
